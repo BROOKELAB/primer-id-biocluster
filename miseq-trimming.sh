88bf3cddb28e06621e045cb134221776ee7f7463
Jessica Holmes <kirk617@gmail.com>
2018-10-29 15:41:33 -0500

    Update to SLURM

--- miseq-trimming.sh
+++ miseq-trimming.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=4
-#PBS -m abe
-#PBS -j oe
-#PBS -M jholmes5@illinois.edu
-#PBS -N trim_arr
-#PBS -t 1-4
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH --mail-user=jholmes5@illinois.edu
+#SBATCH -J trim_arr
+#SBATCH -a 1-4
 
-module load trimmomatic/0.33
+module load Trimmomatic/0.38-Java-1.8.0_152
 
-cd $PBS_O_WORKDIR
+cd $SLURM_SUBMIT_DIR
 
-line=$(sed -n -e "$PBS_ARRAYID p" brooke_basenames.txt)
+line=$(sed -n -e "$SLURM_ARRAY_TASK_ID p" brooke_basenames.txt)
 
-java -jar /home/apps/trimmomatic/trimmomatic-0.33/trimmomatic-0.33.jar PE -threads 4 -phred33  \
+java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.38.jar PE -threads 4 -phred33  \
 ../data/raw-seq/${line}R1_001.fastq ../data/raw-seq/${line}R2_001.fastq ../results/trimmomatic/${line}R1_trim_paired.fastq \
 ../results/trimmomatic/${line}R1_trim_unpaired.fastq ../results/trimmomatic/${line}R2_trim_paired.fastq \
-../results/trimmomatic/${line}R2_trim_unpaired.fastq ILLUMINACLIP:/home/apps/trimmomatic/trimmomatic-0.33/adapters/TruSeq3-PE-2.fa:2:15:10 MINLEN:30
+../results/trimmomatic/${line}R2_trim_unpaired.fastq ILLUMINACLIP:/home/apps/trimmomatic/trimmomatic-0.38/adapters/TruSeq3-PE-2.fa:2:15:10 MINLEN:30
 
