5805eb3132edc9b44df95d99adc14bda6effee27
Jessica Holmes <kirk617@gmail.com>
2018-10-29 15:38:17 -0500

    Updated to SLURM

--- fastqc_brooke.sh
+++ fastqc_brooke.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=1
-#PBS -m abe
-#PBS -j oe
-#PBS -M jholmes5@illinois.edu
-#PBS -N fastqc
-#PBS -t 1-4
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --mem=5G
+#SBATCH --mail-user=jholmes5@illinois.edu
+#SBATCH -J fastqc
+#SBATCH -a 1-4
 
-module load fastqc/0.11.5
+module load FastQC/0.11.5-IGB-gcc-4.9.4-Java-1.8.0_152
 
-cd $PBS_O_WORKDIR
+cd $SLURM_SUBMIT_DIR
 
-line=$(sed -n -e "$PBS_ARRAYID p" brooke_basenames.txt)
+line=$(sed -n -e "$SLURM_ARRAY_TASK_ID p" brooke_basenames.txt)
 
 #Raw reads
 fastqc -outdir=../results/fastqc_raw/ --noextract ../data/raw-seq/${line}R1_001.fastq
 fastqc -outdir=../results/fastqc_raw/ --noextract ../data/raw-seq/${line}R2_001.fastq
-
-#Trimmed reads
-
-
